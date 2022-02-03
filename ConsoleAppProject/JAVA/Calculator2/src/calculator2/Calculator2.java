62d65a6552bb42254a9926695cbce80cafd82ae3
Alex Kennedy <100171777@student.burnley.ac.uk>
2022-02-03 15:25:32 +0000

    completed calc2

--- ConsoleAppProject/JAVA/Calculator2/src/calculator2/Calculator2.java
+++ ConsoleAppProject/JAVA/Calculator2/src/calculator2/Calculator2.java
@@ -7,7 +7,31 @@ package calculator2;
 public class Calculator2
 {
    String Name;
+
    
-   
+   public int add(int x, int y) {
+       return x + y;
+   }
     
-}  
+ 
+
+   
+   public int subtract(int x, int y) {
+       return x - y;
+   }
+
+   
+   public int multiply(int x, int y) {
+       return x * y;
+   }
+
+   
+   public int divide(int x, int y) {
+       return x / y;
+   }
+
+   
+   public int modulus(int x, int y) {
+       return x % y; 
+   }
+}
--- ConsoleAppProject/JAVA/Calculator2/src/calculator2/Main.java
+++ ConsoleAppProject/JAVA/Calculator2/src/calculator2/Main.java
@@ -9,6 +9,14 @@ package calculator2;
  * @author 100171777
  */
 public class Main {
-    public static void
-    
+    public static void main(String[] args){
+            Calculator2 c1 = new Calculator2();
+            Calculator2 c2 = new Calculator2();
+            
+            c1.Name = "Awesomeness";
+            c2.Name = "Bacon, Egg & Cheese";
+            
+            System.out.println(c1.Name + " calculated " + c1.add(3, 4));
+            System.out.println(c2.Name + " calculated " + c2.modulus(29, 5));
+     }
 }
